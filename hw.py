518795abdaa4089f13a4e12e013e99cbe7439ef6
qaaziabdullah-eng <qaaziabdullah@gmail.com>
2026-01-01 18:00:39 +0500

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,11 @@
+#Square
+
+import turtle 
+turtle.Screen().bgcolor("pink")
+turtle.Screen().setup(500, 500)
+turt = turtle.Turtle()
+
+for i in range(4):
+    turt.forward(100)
+    turt.left(90)
+turtle.done()
